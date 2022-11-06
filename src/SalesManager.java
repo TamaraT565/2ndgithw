5d3bcf72a53fe3ab2d5d037df4f8ad57c5506034
Tamara Tatarintseva <tta.nemo@gmail.com>
2022-11-06 22:58:01 +0300

    obrezaverage

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,2 +1,37 @@
 package PACKAGE_NAME;public class SalesManager {
+    protected int[] sales;
+
+    public SalesManager(int[] sales) {
+        this.sales = sales;
+    }
+
+    public int max() {
+        int max = -1;
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+    }
+
+    public int min() {
+        int min = sales[0];
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int ObrezAverage() {
+        int ObrezAverage = 0;
+        int result = 0;
+        for (int d : sales) {
+            result += d;
+            ObrezAverage = (result - max() - min()) / (sales.length - 2);
+        }
+        return ObrezAverage;
+    }
 }
